941bbca7d50d468069279f3de8d480a49e20343b
DiamondOreDev <harsh.diamondore@gmail.com>
2023-09-19 17:00:34 +0530

    Consulting added

--- src/Pages/ConsultingPage.jsx
+++ src/Pages/ConsultingPage.jsx
@@ -3,6 +3,7 @@ import emailjs from "@emailjs/browser";
 import Midas from "../assets/MidasLogo.png";
 import DOC from "../assets/DOC-2.png";
 import MutualFund from "../assets/MutualFundImg.png";
+import { Link } from "react-router-dom";
 
 const ConsultingPage = () => {
     const form = useRef();
@@ -46,167 +47,167 @@ const ConsultingPage = () => {
 
             <div className="mt-8 flex flex-wrap gap-4 text-center">
               <a
-                href="#"
+                href={'/consulting#contact-mutual-funds'}
                 className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
               >
                 Get Started
               </a>
 
-              <a
+              {/* <a
                 href="#"
                 className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
               >
                 Learn More
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
       </section>
-      <div class="bg-white py-6 sm:py-8 lg:py-12">
-        <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
-          <div class="mb-10 md:mb-16">
-            <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
+      <div className="bg-white py-6 sm:py-8 lg:py-12">
+        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+          <div className="mb-10 md:mb-16">
+            <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
               Our Offerings
             </h2>
 
-            {/* <p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">We always welcom</p> */}
+            {/* <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">We always welcom</p> */}
           </div>
-          <div class="grid gap-8 sm:grid-cols-2 md:gap-12 xl:grid-cols-3 xl:gap-16">
-            <div class="flex gap-4 md:gap-6">
-              <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+          <div className="grid gap-8 sm:grid-cols-2 md:gap-12 xl:grid-cols-3 xl:gap-16">
+            <div className="flex gap-4 md:gap-6">
+              <div className="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
+                  className="h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
                   />
                 </svg>
               </div>
 
               <div>
-                <h3 class="mb-2 text-lg font-semibold md:text-xl">
+                <h3 className="mb-2 text-lg font-semibold md:text-xl">
                   Debt Instruments
                 </h3>
-                <p class="mb-2 text-gray-500">
+                <p className="mb-2 text-gray-500">
                   Viz., RBI Taxable Bonds, Tax Free Bonds, NCD (Non Convertible
                   Debentures), Corporate Fixed Deposits
                 </p>
               </div>
             </div>
 
-            <div class="flex gap-4 md:gap-6">
-              <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+            <div className="flex gap-4 md:gap-6">
+              <div className="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
+                  className="h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
                   />
                 </svg>
               </div>
 
               <div>
-                <h3 class="mb-2 text-lg font-semibold md:text-xl">
+                <h3 className="mb-2 text-lg font-semibold md:text-xl">
                   Mutual Funds
                 </h3>
-                <p class="mb-2 text-gray-500">
+                <p className="mb-2 text-gray-500">
                   Help with investing in the top Mutual Funds
                 </p>
               </div>
             </div>
 
-            <div class="flex gap-4 md:gap-6">
-              <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+            <div className="flex gap-4 md:gap-6">
+              <div className="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
+                  className="h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"
                   />
                 </svg>
               </div>
 
               <div>
-                <h3 class="mb-2 text-lg font-semibold md:text-xl">
+                <h3 className="mb-2 text-lg font-semibold md:text-xl">
                   PMS (Portfolio Management Services)
                 </h3>
-                <p class="mb-2 text-gray-500">
+                <p className="mb-2 text-gray-500">
                   Personalized assistance for the management of your portfolio.
                 </p>
               </div>
             </div>
 
-            <div class="flex gap-4 md:gap-6">
-              <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+            <div className="flex gap-4 md:gap-6">
+              <div className="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
+                  className="h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M13 10V3L4 14h7v7l9-11h-7z"
                   />
                 </svg>
               </div>
 
               <div>
-                <h3 class="mb-2 text-lg font-semibold md:text-xl">
+                <h3 className="mb-2 text-lg font-semibold md:text-xl">
                   Goal Based Financial Planning
                 </h3>
-                <p class="mb-2 text-gray-500">
+                <p className="mb-2 text-gray-500">
                   Viz. Child Education Planning, Marriage Planning, Retirement
                   Planning
                 </p>
               </div>
             </div>
 
-            <div class="flex gap-4 md:gap-6">
-              <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+            <div className="flex gap-4 md:gap-6">
+              <div className="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
+                  className="h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"
                   />
                 </svg>
               </div>
 
               <div>
-                <h3 class="mb-2 text-lg font-semibold md:text-xl">Insurance</h3>
-                <p class="mb-2 text-gray-500">
+                <h3 className="mb-2 text-lg font-semibold md:text-xl">Insurance</h3>
+                <p className="mb-2 text-gray-500">
                   New age ULIP, Life Insurance, Health Insurance, General
                   Insurance
                 </p>
@@ -216,7 +217,7 @@ const ConsultingPage = () => {
             {/* <div class="flex gap-4 md:gap-6">
         <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
           </svg>
         </div>
 
@@ -230,7 +231,7 @@ const ConsultingPage = () => {
         </div>
       </div>
       {/* Contact Start */}
-      <section className="bg-gray-50">
+      <section id="contact-mutual-funds" className="bg-gray-50">
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-800">
             Contact Us
