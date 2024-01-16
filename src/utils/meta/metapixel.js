9cb27c765e20796ff4e6c0e5ef7acb35df91bf07
DiamondOreDev <harsh.diamondore@gmail.com>
2024-01-16 11:24:46 +0530

    pixel code

---
+++ src/utils/meta/metapixel.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const MetaPixel = () => {
+    const renderMetaPixel = () => {
+        const scripts = [];
+    
+        scripts.push(
+            React.createElement('script', { key: 'function', id: 'facebook-pixel-script' }, `!(function (f, b, e, v, n, t, s) {
+                if (f.fbq) return;
+                n = f.fbq = function () {
+                  n.callMethod
+                    ? n.callMethod.apply(n, arguments)
+                    : n.queue.push(arguments);
+                };
+                if (!f._fbq) f._fbq = n;
+                n.push = n;
+                n.loaded = !0;
+                n.version = "2.0";
+                n.queue = [];
+                t = b.createElement(e);
+                t.async = !0;
+                t.src = v;
+                s = b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t, s);
+              })(
+                window,
+                document,
+                "script",
+                "https://connect.facebook.net/en_US/fbevents.js"
+              );
+              fbq("init", "935198161540004");
+              fbq("track", "PageView");`)
+        );
+    
+        scripts.push(
+            React.createElement('noscript', { key: 'image', id: 'facebook-pixel-image' }, 'YOUR_IMAGE')
+        );
+    
+        return scripts;
+    };
+  return (
+    <Helmet>
+        {renderMetaPixel}
+    </Helmet>
+  );
+};
+
+export default MetaPixel;
